064f38129d8b4946ad46895d5b062ddc60aed8e0
humulla <humairaa.mulla@hackney.gov.uk>
2021-08-19 14:35:35 +0100

    updated terraform

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -14,7 +14,6 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
-   application_name = postgres training # The name to use for your application
    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
