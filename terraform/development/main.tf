6b9882b51158c2e6a01d471900ac68a240e96444
humulla <humairaa.mulla@hackney.gov.uk>
2021-08-23 12:43:09 +0100

    terraform update!

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -43,7 +43,7 @@ data "aws_subnet_ids" "development_private_subnets" {
 module "postgres_db_development" {
   source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git/modules/database/postgres"
   environment_name = "development"
-  vpc_id = data.aws_subnet_ids.vpc_id
+  vpc_id = data.aws_subnet_ids.development_private_subnets.vpc_id
   db_engine = "postgres"
   db_engine_version = "11.1"
   db_identifier = "postgres-training-dev-db"
