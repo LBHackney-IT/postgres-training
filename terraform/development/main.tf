a16fd54ddd76050046f129fbee3c77b4930df3f4
humulla <humairaa.mulla@hackney.gov.uk>
2021-08-23 12:15:33 +0100

    terraform update

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -17,14 +17,13 @@ locals {
    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
+data "aws_iam_role" "ec2_container_service_role" {
+  name = "ecsServiceRole"
+}
 
-# data "aws_iam_role" "ec2_container_service_role" {
-#  name = "ecsServiceRole"
-# }
-
-# data "aws_iam_role" "ecs_task_execution_role" {
-#  name = "ecsTaskExecutionRole"
-# }
+data "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+}
 
 terraform {
   backend "s3" {
@@ -36,36 +35,36 @@ terraform {
 }
 
 /*    POSTGRES SET UP    */
-# data "aws_vpc" "development_vpc" {
-#  tags = {
-#    Name = "vpc-development-apis-development"
-#  }
-# }
-# data "aws_subnet_ids" "development_private_subnets" {
-#  vpc_id = data.aws_vpc.development_vpc.id
-#  filter {
-#    name   = "tag:Type"
-#    values = ["private"]
-#  }
-# }
+data "aws_vpc" "development_vpc" {
+  tags = {
+       Name = "vpc-development-apis-development"
+  }
+}
+data "aws_subnet_ids" "development_private_subnets" {
+ vpc_id = data.aws_vpc.development_vpc.id
+ filter {
+   name   = "tag:Type"
+   values = ["private"]
+  }
+}
 
-# module "postgres_db_development" {
-#  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git/modules/database/postgres"
-#  environment_name = "development"
-#  vpc_id = data.aws_vpc.development_vpc.id
-#  db_engine = "postgres"
-#  db_engine_version = "11.1"
-#  db_identifier = "postgres-training-dev-db"
-#  db_instance_class = "db.t2.micro"
-#  db_name = "postgres-training-dev"
-#  db_port  = 5002
-#  db_username = "${local.parameter_store}/postgres-training-api/development/postgres-username"
-#  db_password = "${local.parameter_store}/postgres-training-api/development/postgres-password"
-#  subnet_ids = data.aws_subnet_ids.development_private_subnets.ids
-#  db_allocated_storage = 20
-#  maintenance_window ="sun:10:00-sun:10:30"
-#  storage_encrypted = false
-#  multi_az = false //only true if production deployment
-#  publicly_accessible = false
-#  project_name = "platform apis"
-# }
+module "postgres_db_development" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git/modules/database/postgres"
+  environment_name = "development"
+  vpc_id = data.aws_vpc.development_vpc.id
+  db_engine = "postgres"
+  db_engine_version = "11.1"
+  db_identifier = "postgres-training-dev-db"
+  db_instance_class = "db.t2.micro"
+  db_name = "postgres-training-dev"
+  db_port  = 5002
+  db_username = "${local.parameter_store}/postgres-training-api/development/postgres-username"
+  db_password = "${local.parameter_store}/postgres-training-api/development/postgres-password"
+  subnet_ids = data.aws_subnet_ids.development_private_subnets.ids
+  db_allocated_storage = 20
+  maintenance_window ="sun:10:00-sun:10:30"
+  storage_encrypted = false
+  multi_az = false //only true if production deployment
+  publicly_accessible = false
+  project_name = "platform apis"
+}
